a378a7f45e91ea25a1d52863f7ea6f6e4c5e5520
Andy Chen <andycandomore@gmail.com>
2019-08-14 21:47:14 -0700

    update default

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-const logger = (mode='dev') =>{
+const logger = (mode = 'dev') => {
+  const t = new Date()
+  const timestamp = t.toLocaleTimeString('en-US', { hour12: false })
   switch (mode) {
     case 'dev':
       return (req, res, next) => {
-        const now = process.hrtime();
         res.on('finish', () => {
-          const current = process.hrtime()
-          const period = ((current[1] - now[1])/10000000).toFixed(3);
-          const t = new Date();
-          const timestamp = t.toLocaleTimeString('en-US', {hour12: false});
-          let color = "\x1b[1m"
-          const resetColor = "\x1b[0m"
-          if(res.statusCode<=300 && res.statusCode>100)color = "\x1b[32m";
-          if(res.statusCode>400) color = "\x1b[33m";
-          if(res.statusCode>=500) color = "\x1b[31m";
+          let color = '\x1b[1m'
+          const resetColor = '\x1b[0m'
+          if (res.statusCode <= 300 && res.statusCode > 100)color = '\x1b[32m'
+          if (res.statusCode > 400) color = '\x1b[33m'
+          if (res.statusCode >= 500) color = '\x1b[31m'
           console.info(timestamp, color, res.statusCode, resetColor, req.method, req.url)
         })
-        next();
+        next()
       }
-      break;
     default:
-      next();
-      break;
+      return (req, res, next) => {
+        console.info(timestamp, res.statusCode, req.method, req.url)
+        next()
+      }
   }
 }
 
-module.exports = logger;
+module.exports = logger
