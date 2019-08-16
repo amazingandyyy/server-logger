03475e0d62dbfbb04828879d95c74008ac43e7c1
Andy Chen <andycandomore@gmail.com>
2019-08-15 21:20:37 -0700

    yellow out for >= 400 statusCode

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const logger = (mode = 'dev') => {
           let color = '\x1b[1m'
           const resetColor = '\x1b[0m'
           if (res.statusCode <= 300 && res.statusCode > 100)color = '\x1b[32m'
-          if (res.statusCode > 400) color = '\x1b[33m'
+          if (res.statusCode >= 400) color = '\x1b[33m'
           if (res.statusCode >= 500) color = '\x1b[31m'
           console.info(timestamp, color, res.statusCode, resetColor, req.method, req.url)
         })
